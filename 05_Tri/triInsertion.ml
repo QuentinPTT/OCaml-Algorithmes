8db78e84f16bc0201e962933e6b09533aa74ccba
Quentin Petit <quentin.abo@hotmail.com>
2021-04-28 21:29:36 +0200

    Update triInsertion.ml

--- 05_Tri/triInsertion.ml
+++ 05_Tri/triInsertion.ml
@@ -6,29 +6,29 @@ let liste = [10;3;6;2;4;1];;
 (* A - Version Tableau *)
 
 let triInsertionTab tab =
-		let n = Array.length(tab) in
-		for k = 1 to n-1 do
-				let x = tab.(k) in
-				let j = ref(k-1) in
-				while !j>=0 && tab.(!j) > x do
-						tab.(!j+1)<-tab.(!j);
-						decr j;
-				done;
-				tab.(!j+1)<-x;
+	let n = Array.length(tab) in
+	for k = 1 to n-1 do
+		let x = tab.(k) in
+		let j = ref(k-1) in
+		while !j>=0 && tab.(!j) > x do
+			tab.(!j+1)<-tab.(!j);
+			decr j;
 		done;
-		tab;;
+		tab.(!j+1)<-x;
+	done;
+	tab;;
 		
 triInsertionTab tableau;;
 
 (* B - Version Liste *)
 
 let rec insertion x l = match l with
-		|[] -> [x]
-		|t::q when x<t -> x::l
-	  |t::q -> t::(insertion x q);;
+	|[] -> [x]
+	|t::q when x<t -> x::l
+	|t::q -> t::(insertion x q);;
 	 
 let rec triListe l = match l with
-		|[] -> []
-		|t::q -> insertion t (triListe q);;
+	|[] -> []
+	|t::q -> insertion t (triListe q);;
 		
 triListe liste;;
