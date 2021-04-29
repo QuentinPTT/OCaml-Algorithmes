e5021e3ec225baa914111bb68995dca4c56ee243
Quentin Petit <quentin.abo@hotmail.com>
2021-04-29 13:21:45 +0200

    Update calculSomme.ml

--- 02_Récursivité/calculSomme.ml
+++ 02_Récursivité/calculSomme.ml
@@ -3,13 +3,13 @@
 (* A - Algorithme récursif classique *)
 
 let rec som n = match n with
-		1 -> 1
-		|n -> n + som(n-1);;
+	1 -> 1
+	|n -> n + som(n-1);;
 
 (* B - Algorithme récursif terminal *)
 
 let som n =
-		let rec aux k s = match k with
-				1 -> s
-				|k -> aux(k-1) (s+k)
-		in aux n 1 ;;
+	let rec aux k s = match k with
+		1 -> s
+		|k -> aux(k-1) (s+k)
+	in aux n 1 ;;
