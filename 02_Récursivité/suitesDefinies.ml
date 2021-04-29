16a10d6d5405e3ceeb9e3c4394bed347a9457285
Quentin Petit <quentin.abo@hotmail.com>
2021-04-29 13:21:58 +0200

    Update suitesDefinies.ml

--- 02_Récursivité/suitesDefinies.ml
+++ 02_Récursivité/suitesDefinies.ml
@@ -1,8 +1,8 @@
 (* Deux suites définies entre-elles *)
 
 let rec u = function
-		|0 -> 1
-		|n -> v(n-1)+1
+	|0 -> 1
+	|n -> v(n-1)+1
 and v = function
-		|0 -> 2
-		|n -> u(n-1)+3
+	|0 -> 2
+	|n -> u(n-1)+3
